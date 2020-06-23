12086912490fb54def2d14690b8f49711db0ac1e
Dennis James Matildo <dj.matildo@gmail.com>
2020-06-23 12:39:33 +0800

    Manage local stateManage local state

--- start/client/src/index.tsx
+++ start/client/src/index.tsx
@@ -1,11 +1,16 @@
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
-import { ApolloProvider } from '@apollo/react-hooks';
+import { ApolloProvider, useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 import React from 'react';
 import ReactDOM from 'react-dom'; 
+import { resolvers, typeDefs } from './resolvers';
+
 import Pages from './pages';
+import Login from './pages/login';
 import injectStyles from './styles';
+
 const cache = new InMemoryCache();
 
 const link = new HttpLink({
@@ -24,28 +29,26 @@ cache.writeData({
 
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   cache,
-  link
+  link,
+  resolvers,
+  typeDefs,
 });
+
+const IS_LOGGED_IN = gql`
+  query IsUserLoggedIn {
+    isLoggedIn @client
+  }
+`;
+
+function IsLoggedIn() {
+  const { data } = useQuery(IS_LOGGED_IN);
+  return data.isLoggedIn ? <Pages /> : <Login />;
+}
 
 injectStyles();
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Pages />
-  </ApolloProvider>, 
-  document.getElementById('root')
+    <IsLoggedIn />
+  </ApolloProvider>,
+  document.getElementById('root'),
 );
-
-// client
-//   .query({
-//     query: gql`
-//       query GetLaunch {
-//         launch(id: 56) {
-//           id
-//           mission {
-//             name
-//           }
-//         }
-//       }
-//     `
-//   })
-//   .then(result => console.log(result));
