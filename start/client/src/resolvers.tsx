80be19c2987af9c527c742ba1ce0fff5be81997d
Dennis James Matildo <dj.matildo@gmail.com>
2020-06-24 15:00:58 +0800

    Code Cleanup

--- start/client/src/resolvers.tsx
+++ start/client/src/resolvers.tsx
@@ -44,32 +44,35 @@ interface AppResolvers extends Resolvers {
 export const resolvers: AppResolvers = {  
     Launch: {
         isInCart: (launch: LaunchTileTypes.LaunchTile, _, { cache }): boolean => {
-        const queryResult = cache.readQuery<GetCartItemTypes.GetCartItems>({ 
-            query: GET_CART_ITEMS 
-        });
-        if (queryResult) {
-            return queryResult.cartItems.includes(launch.id)
-        } 
-        return false;
+          const queryResult = cache.readQuery<GetCartItemTypes.GetCartItems>({ 
+              query: GET_CART_ITEMS 
+          });
+
+          if (queryResult) {
+              return queryResult.cartItems.includes(launch.id)
+          } 
+          return false;
         }
     },
     Mutation: {
         addOrRemoveFromCart: (_, { id }: { id: string }, { cache }): string[] => {
-        const queryResult = cache
-            .readQuery<GetCartItemTypes.GetCartItems, any>({ 
+          const queryResult = cache.readQuery<GetCartItemTypes.GetCartItems, any>({ 
             query: GET_CART_ITEMS 
-            });
-        if (queryResult) {
-            const { cartItems } = queryResult;
-            const data = {
-            cartItems: cartItems.includes(id)
-                ? cartItems.filter((i) => i !== id)
-                : [...cartItems, id],
-            };
-            cache.writeQuery({ query: GET_CART_ITEMS, data });
-            return data.cartItems;
-        }
-        return [];
+          });
+
+          if (queryResult) {
+              const { cartItems } = queryResult;
+              const data = {
+              cartItems: cartItems.includes(id)
+                  ? cartItems.filter((i) => i !== id)
+                  : [...cartItems, id],
+              };
+              cache.writeQuery({ query: GET_CART_ITEMS, data });
+              
+              return data.cartItems;
+          }
+          
+          return [];
         },
     },
 };
